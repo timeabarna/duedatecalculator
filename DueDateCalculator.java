a263e600afbe550527b522558af6edc5d63ed1f1
timeabarna <38653567+timeabarna@users.noreply.github.com>
2020-08-14 15:13:47 +0200

    Add files via upload
    Add eigthy hour test case and repare bug

---
+++ DueDateCalculator.java
@@ -0,0 +1,84 @@
+package service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.EnumSet;
+
+public class DueDateCalculator {
+    private static final LocalTime NINE_O_CLOCK= LocalTime.of(9, 0, 0);
+    private static final LocalTime FIVE_O_CLOCK= LocalTime.of(17, 0, 0);
+    private static final EnumSet<DayOfWeek> WEEKEND = EnumSet.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+    private static final long NON_WORK_HOURS= 16L;
+    private static final long NON_WORKING_DAYS = 48L;
+
+    public LocalDateTime calculateDueDate(LocalDateTime submissionDate, long turnaroundTime) {
+        validateSubmissionDate(submissionDate);
+        validateTurnaroundTime(turnaroundTime);
+        return calculate(submissionDate, turnaroundTime);
+    }
+
+    private LocalDateTime calculate(LocalDateTime submissionDate, long turnaroundTime) {
+        LocalDateTime dateWithTurnaroundAdded = submissionDate;
+
+        for (int i = 0; i < turnaroundTime; i++) {
+            dateWithTurnaroundAdded = dateWithTurnaroundAdded.plusHours(1L);
+            dateWithTurnaroundAdded = shiftWorkday(dateWithTurnaroundAdded);
+            dateWithTurnaroundAdded = shiftWeekend(dateWithTurnaroundAdded);
+        }
+            return dateWithTurnaroundAdded;
+        }
+
+    private LocalDateTime shiftWorkday(LocalDateTime dateWithTurnaroundAdded) {
+        if (!isBetweenNineAndFive(dateWithTurnaroundAdded)) {
+            dateWithTurnaroundAdded = dateWithTurnaroundAdded.plusHours(NON_WORK_HOURS);
+        }
+        return dateWithTurnaroundAdded;
+    }
+
+    private LocalDateTime shiftWeekend(LocalDateTime dateWithTurnaroundAdded) {
+        if (isWeekend(dateWithTurnaroundAdded)) {
+            dateWithTurnaroundAdded = dateWithTurnaroundAdded.plusHours(NON_WORKING_DAYS);
+        }
+        return dateWithTurnaroundAdded;
+    }
+
+    private void validateSubmissionDate(LocalDateTime submissionDate) {
+        isNullDateTime(submissionDate);
+        validateIsWeekend(submissionDate);
+        validateIsBetweenNineAndFive(submissionDate);
+    }
+
+    private void validateTurnaroundTime(long turnaroundTime) {
+        if (turnaroundTime <= 0) {
+            throw new IllegalArgumentException("Turnaround time must be greater than 0");
+        }
+    }
+
+    private void isNullDateTime(LocalDateTime submissionDate) {
+        if (null == submissionDate) {
+            throw new IllegalArgumentException("Submission date can not be null");
+        }
+    }
+
+    private boolean isWeekend(LocalDateTime dateTobeValidated) {
+        return WEEKEND.contains(dateTobeValidated.getDayOfWeek());
+    }
+
+    private boolean isBetweenNineAndFive(LocalDateTime dateToBeValidated) {
+        LocalTime timeFromSubmissionDate = dateToBeValidated.toLocalTime();
+        return !timeFromSubmissionDate.isBefore(NINE_O_CLOCK) && !timeFromSubmissionDate.isAfter(FIVE_O_CLOCK);
+    }
+
+    private void validateIsWeekend(LocalDateTime dateTobeValidated) {
+        if (isWeekend(dateTobeValidated)) {
+            throw new IllegalArgumentException("Submission date must fall on the weekdays");
+        }
+    }
+
+    private void validateIsBetweenNineAndFive(LocalDateTime dateToBeValidated) {
+        if (!isBetweenNineAndFive(dateToBeValidated)) {
+            throw new IllegalArgumentException("Submission hour must be between 9 AM and 5 PM");
+        }
+    }
+}
